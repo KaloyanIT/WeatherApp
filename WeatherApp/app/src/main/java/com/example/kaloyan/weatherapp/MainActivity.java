94a0aaec37f11156952b22f5a499ff02d2ff81f4
Kaloyan <kaloyanit@yahoo.com>
2017-04-05 01:04:31 +0300

    Change colours

--- WeatherApp/app/src/main/java/com/example/kaloyan/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/kaloyan/weatherapp/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.kaloyan.weatherapp;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import okhttp3.OkHttpClient;
 
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OkHttpClient client = new OkHttpClient();
     }
 }
