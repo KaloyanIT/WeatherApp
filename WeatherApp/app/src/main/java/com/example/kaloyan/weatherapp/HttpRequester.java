801312ba081ab46561fb9deaacb76ca53ebd3f40
Kaloyan <kaloyanit@yahoo.com>
2017-04-05 22:33:34 +0300

    Add HttpRequester getJSON Method

---
+++ WeatherApp/app/src/main/java/com/example/kaloyan/weatherapp/HttpRequester.java
@@ -0,0 +1,38 @@
+package com.example.kaloyan.weatherapp;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by kaloyan on 05.04.17.
+ */
+
+public class HttpRequester {
+    private final OkHttpClient httpClient;
+
+    public HttpRequester() {
+        this(new OkHttpClient());
+    }
+
+
+    public HttpRequester(OkHttpClient okHttpClient) {
+        this.httpClient = new OkHttpClient();
+    }
+
+    public String getJson(String url) throws IOException {
+        Request request = new Request.Builder()
+                .get()
+                .url(url)
+                .build();
+
+        Response response = this.httpClient.newCall(request)
+                .execute();
+
+        //TODO: Add error handling 401 and so on
+
+        return response.body().string();
+    }    
+}
--- WeatherApp/app/src/main/java/com/example/kaloyan/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/kaloyan/weatherapp/MainActivity.java
@@ -11,6 +11,5 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        OkHttpClient client = new OkHttpClient();
     }
 }
