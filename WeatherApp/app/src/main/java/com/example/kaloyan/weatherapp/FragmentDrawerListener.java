428c34d20a272636038195d630e44ef5ef9b6e21
Kaloyan <kaloyanit@yahoo.com>
2017-04-09 23:24:58 +0300

    Add navigation drawer fragment and recyclerView Adapter

---
+++ WeatherApp/app/src/main/java/com/example/kaloyan/weatherapp/FragmentDrawerListener.java
@@ -0,0 +1,11 @@
+package com.example.kaloyan.weatherapp;
+
+import android.view.View;
+
+/**
+ * Created by kaloyan on 09.04.17.
+ */
+
+interface FragmentDrawerListener {
+    public void onDrawerItemSelected(View view, int position);
+}
--- WeatherApp/app/src/main/java/com/example/kaloyan/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/kaloyan/weatherapp/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.kaloyan.weatherapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -20,8 +22,10 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
     private Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
 
 
         WeatherData data = new WeatherData(url);
@@ -59,5 +67,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+
     }
 }
